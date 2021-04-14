f46f05fcb439ca50e5234a35efc859cbab598b51
DESKTOP-PNQ3FLQ\jahhy <jah.hyun12345@gmail.com>
2021-04-14 11:16:36 +0900

    * mongoDB 연결

--- index.tsx
+++ index.tsx
@@ -2,6 +2,13 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://jahyun12345:qlalfqjsgh12@jahyun12345.ijjvj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+    // 에러방지 
+    useNewUrlParrser: true, useUnifiedTopology:true, useCreateIndex:true, useFindAndModify:false
+}).then(() => console.log('MongoDB Connected!')).catch(err => console.log(err))
+
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(port, () => console.log('Example app listening on port' + port + '!'))
